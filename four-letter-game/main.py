b2c25114925b0bf5cb075855320335e5df31b920
Byun Kyung MIn <chgemn@gmail.com>
2023-02-24 01:54:27 +0900

    Add more data, fix list index error

--- four-letter-game/main.py
+++ four-letter-game/main.py
@@ -8,9 +8,13 @@ import os, random
 img_file_names = []
 quizes = []
 
-for filename in os.listdir("four-letter-game/data/imgs"):
-    if filename not in img_file_names:
-        img_file_names.append(filename)
+
+def get_img_list():
+    global img_file_names
+    for filename in os.listdir("four-letter-game/data/imgs"):
+        if filename not in img_file_names:
+            img_file_names.append(filename)
+
 
 def read_data():
     global quizes
@@ -21,43 +25,31 @@ def read_data():
     for line in lines:
         quizes.append(line)
 
+def merge_quizes():
+    global quizes
+    quizes += img_file_names
+    random.shuffle(quizes)
+
 def ppt_gen():
     prs = Presentation()
-    size = len(quizes)+len(img_file_names)
-    for i in range(size):
+    for quiz in list(quizes):
         slide_layout = prs.slide_layouts[6]
         slide = prs.slides.add_slide(slide_layout)
 
-        tmp = random.randint(6,10)
-        if i%tmp == 0:
-            if not len(img_file_names) == 0:
-                img_path = random.choice(img_file_names)
-                pic = slide.shapes.add_picture("four-letter-game/data/imgs/"+img_path, Inches(1), Inches(1), height=Inches(5))
-                pic.left = int((prs.slide_width - pic.width) / 2)
-                pic.top = int((prs.slide_height - pic.height) / 2)
-                img_file_names.remove(img_path)
-            else:
-                # add 4글자 문제
-                txBox = slide.shapes.add_textbox(Inches(1), Inches(2), Inches(8), Inches(2))
-                tf = txBox.text_frame
-                randomQuiz = random.choice(quizes)
-                tf.text = randomQuiz[0:2]
-                
-                tf.paragraphs[0].font.size = Pt(100)
-                tf.paragraphs[0].font.bold = True
+        if quiz[-3:] == 'png' or quiz[-3:] == 'jpg':
+            try:
+                pic = slide.shapes.add_picture("four-letter-game/data/imgs/"+quiz, Inches(1), Inches(1), height=Inches(5))
+            except:
+                print(quiz)
+            pic.left = int((prs.slide_width - pic.width) / 2)
+            pic.top = int((prs.slide_height - pic.height) / 2)
+            quizes.remove(quiz)
 
-                tf.paragraphs[0].alignment = PP_ALIGN.CENTER
-                txBox.text_frame.vertical_anchor = MSO_VERTICAL_ANCHOR.MIDDLE
-                tf.auto_size = MSO_AUTO_SIZE.SHAPE_TO_FIT_TEXT
-
-                tf.vertical_anchor = MSO_ANCHOR.MIDDLE
-                quizes.remove(randomQuiz)  
         else:
             # add 4글자 문제
             txBox = slide.shapes.add_textbox(Inches(1), Inches(2), Inches(8), Inches(2))
             tf = txBox.text_frame
-            randomQuiz = random.choice(quizes)
-            tf.text = randomQuiz[0:2]
+            tf.text = quiz[0:2]
             
             tf.paragraphs[0].font.size = Pt(100)
             tf.paragraphs[0].font.bold = True
@@ -67,9 +59,19 @@ def ppt_gen():
             tf.auto_size = MSO_AUTO_SIZE.SHAPE_TO_FIT_TEXT
 
             tf.vertical_anchor = MSO_ANCHOR.MIDDLE
-            quizes.remove(randomQuiz)
+            quizes.remove(quiz)  
+        
     
-    prs.save("4글자 이어말하기.pptx")
+    prs.save("4글자 이어말하기+인물퀴즈.pptx")
+
+
 
 read_data()
+get_img_list()
+
+merge_quizes()
+print("총", len(img_file_names),"개의 인물사진")
+print("총", len(quizes),"개의 Slides")
 ppt_gen()
+
+
